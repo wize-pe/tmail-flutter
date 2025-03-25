6bfc4e7c14006c44afdd82b56e26a752c3440fbc
dab246 <tdvu@linagora.com>
2025-03-25 17:43:10 +0700

    TF-3358 Show list draft locally dialog

--- lib/main/localizations/app_localizations.dart
+++ lib/main/localizations/app_localizations.dart
@@ -5160,4 +5160,25 @@ class AppLocalizations {
       name: 'spamReportToggleDescription',
     );
   }
+
+  String get localDraftList {
+    return Intl.message(
+      'Local Draft List',
+      name: 'localDraftList',
+    );
+  }
+
+  String get openAll {
+    return Intl.message(
+      'Open all',
+      name: 'openAll',
+    );
+  }
+
+  String get discardAll {
+    return Intl.message(
+      'Discard all',
+      name: 'discardAll',
+    );
+  }
 }
